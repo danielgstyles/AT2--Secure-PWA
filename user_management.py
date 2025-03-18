6e6661ff86787a36ff6d66e9f6caf792b635680d
AceProgrammer2 <daniel.g.styles@gmail.com>
2025-03-18 20:18:07 +1100

    Added placeholders to the SQL Execusions

--- user_management.py
+++ user_management.py
@@ -1,28 +1,49 @@
 import sqlite3 as sql
 import time
 import random
+import bcrypt
 
 
 def insertUser(username, password, DoB):
     con = sql.connect("database_files/database.db")
     cur = con.cursor()
+    
+    encodedPassword = password.encode('utf-8')
+
+    hashedPassword = bcrypt.hashpw(encodedPassword, bcrypt.gensalt())
+
+    print("This the hashed password at submission: " + str(hashedPassword))
     cur.execute(
         "INSERT INTO users (username,password,dateOfBirth) VALUES (?,?,?)",
-        (username, password, DoB),
+        (username, hashedPassword, DoB),
     )
     con.commit()
     con.close()
 
 
 def retrieveUsers(username, password):
+    bytePassword = bytes(password,"utf-8")
     con = sql.connect("database_files/database.db")
     cur = con.cursor()
-    cur.execute(f"SELECT * FROM users WHERE username = '{username}'")
+    ##cur.execute("""SELECT * FROM users WHERE username = ? """, (username))
+    query = "SELECT * FROM users WHERE username = ?"
+    cur.execute(query, (username,))
     if cur.fetchone() == None:
         con.close()
         return False
     else:
-        cur.execute(f"SELECT * FROM users WHERE password = '{password}'")
+        cur.execute(f"SELECT password FROM users WHERE username = '{username}'")
+        hashedPassword = cur.fetchone()
+##        hashedPassword = cur.fetchall()
+        print("This the hashed password converted to a string for printing: " + str(hashedPassword))
+##        hashedPassword = hashedPassword.replace("[(b'", "")
+##        hashedPassword = hashedPassword.replace("',)]", "")
+
+##        print("This the hashed password: " + hashedPassword)
+##        hashedPassword = hashedPassword.encode("utf-8")
+
+        
+        #cur.execute(f"SELECT * FROM users WHERE password = '{password}'")
         # Plain text log of visitor count as requested by Unsecure PWA management
         with open("visitor_log.txt", "r") as file:
             number = int(file.read().strip())
@@ -31,12 +52,19 @@ def retrieveUsers(username, password):
             file.write(str(number))
         # Simulate response time of heavy app for testing purposes
         time.sleep(random.randint(80, 90) / 1000)
-        if cur.fetchone() == None:
-            con.close()
-            return False
-        else:
+        
+##        print(bcrypt.checkpw(password.encode(), hashedPassword.encode()))
+        if bcrypt.checkpw(password.encode(), hashedPassword[0]):
             con.close()
             return True
+        else:
+
+            hashedEnteredPassword = bcrypt.hashpw(b"password", bcrypt.gensalt())
+            print("This is the Hash of the password just entered: " + str(hashedEnteredPassword))
+            print("This the Saved hashed password: " + str(hashedPassword))
+            con.close()
+            return False
+
 
 
 def insertFeedback(feedback):
